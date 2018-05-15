b9d5c27103af21cbd70944cefac9f980eedf7ee3
Geno Roupsky <geno@roupsky.name>
2018-05-15 11:09:13 +0300

    Deploy only one build and migrate db

--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,34 @@
-exports['default'] = {
+exports[ 'default' ] = {
   routes: (api) => {
     return {
 
-      /* ---------------------
-      routes.js
-
-      For web clients (http and https) you can define an optional RESTful mapping to help route requests to actions.
-      If the client doesn't specify and action in a param, and the base route isn't a named action, the action will attempt to be discerned from this routes.js file.
-
-      Learn more here: http://www.actionherojs.com/docs/#routes
-
-      examples:
-
       get: [
-        { path: '/users', action: 'usersList' }, // (GET) /api/users
-        { path: '/search/:term/limit/:limit/offset/:offset', action: 'search' }, // (GET) /api/search/car/limit/10/offset/100
+        { path: '/user', action: 'user:list' },
+        { path: '/me', action: 'user:me' },
+        { path: '/user/:id', action: 'user:show' },
+        { path: '/status', action: 'status' }
       ],
 
       post: [
-        { path: '/login/:userID(^\\d{3}$)', action: 'login' } // (POST) /api/login/123
+        { path: '/session', action: 'session:auth' },
+        { path: '/user', action: 'user:create' },
+        { path: '/user/:id', action: 'user:edit' }
+        // { path: '/session/:email/resetpw', action: 'user:lost' },
+        // { path: '/session/:email/resetpw2', action: 'user:reset' }
       ],
 
-      all: [
-        { path: '/user/:userID', action: 'user', matchTrailingPathParts: true } // (*) /api/user/123, api/user/123/stuff
+      put: [
+        // { path: '/session', action: 'session:check' }
+      ],
+
+      patch: [
+        { path: '/user/:id', action: 'user:changePassword' }
+      ],
+
+      delete: [
+        // { path: '/session', action: 'session:destroy' },
+        { path: '/user/:id', action: 'user:destroy' }
       ]
-
-      ---------------------- */
-
     }
   }
 }
