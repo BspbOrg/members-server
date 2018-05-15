32948cacb54b2bd6409361c496feb51aa141af31
Geno Roupsky <geno@roupsky.name>
2018-05-15 13:04:23 +0300

    Update cors setup

---
+++ config/cors.js
@@ -0,0 +1,26 @@
+exports[ 'default' ] = {
+  cors: () => {
+    return {
+      // Should the plugin be enabled
+      enabled: false,
+      // List of origins that are allowed, all the rest will get `null` as `Access-Control-Allow-Origin`
+      allowedOrigins: [ 'http://localhost:5000' ]
+    }
+  }
+}
+
+exports.staging = {
+  cors: () => {
+    return {
+      allowedOrigins: '*'
+    }
+  }
+}
+
+exports.production = {
+  cors: () => {
+    return {
+      enabled: true
+    }
+  }
+}
--- config/plugins.js
+++ config/plugins.js
@@ -3,6 +3,7 @@ const path = require('path')
 exports[ 'default' ] = {
   plugins: (api) => {
     return {
+      'ah-cors-plugin': { path: path.join(__dirname, '..', 'node_modules', 'ah-cors-plugin') },
       'ah-sequelize-plugin': { path: path.join(__dirname, '..', 'node_modules', 'ah-sequelize-plugin') }
     }
   }
