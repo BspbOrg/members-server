a7eeec79b721b5a9be5f9de1711c399a5f78e339
Geno Roupsky <geno@roupsky.name>
2018-05-22 15:49:05 +0300

    Fix fixture date to always use UTC

--- test/fixtures/02-members.js
+++ test/fixtures/02-members.js
@@ -7,7 +7,7 @@ module.exports = [
       lastName: 'Member',
       username: 'member',
       email: 'member@bspb.org',
-      membershipExpire: new Date(9999, 11, 30, 23, 59, 59, 999),
+      membershipExpire: new Date(Date.UTC(9999, 11, 30, 23, 59, 59, 999)),
       originalSource: 'fixtures',
       accessId: '543210',
       cardId: '12345',
