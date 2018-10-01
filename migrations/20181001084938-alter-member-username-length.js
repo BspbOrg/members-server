e7d8f25fde64c81a32dbcc8f6b5c5d8d39c2d36f
Geno Roupsky <geno@roupsky.name>
2018-10-01 12:36:22 +0300

    Fix lint errors

--- migrations/20181001084938-alter-member-username-length.js
+++ migrations/20181001084938-alter-member-username-length.js
@@ -3,16 +3,15 @@
 module.exports = {
   up: async (queryInterface, DataTypes) => {
     await queryInterface.changeColumn('members', 'username', {
-        type: DataTypes.STRING(50),
-        allowNull: true
-      })
+      type: DataTypes.STRING(50),
+      allowNull: true
+    })
   },
 
   down: async (queryInterface, DataTypes) => {
     await queryInterface.changeColumn('members', 'username', {
-        type: DataTypes.STRING(20),
-        allowNull: true
-      })
+      type: DataTypes.STRING(20),
+      allowNull: true
+    })
   }
 }
-
