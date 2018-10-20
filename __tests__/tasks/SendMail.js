d07c5bbde29defab78e5afb2d5eabf19ed9b5d99
Dimitar Nikolov <d.nikolov@windowslive.com>
2018-10-20 13:39:27 +0300

    integrate mailgun, sendMail task

---
+++ __tests__/tasks/SendMail.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const ah = require('../../test/ah-setup')
+const _ = require('lodash')
+
+const expectedError = new Error('Invalid options. Must contain template, mail, and locals property')
+const params = {
+  template: 'register',
+  mail: {
+    to: 'test@test.tst',
+    subject: 'Helllooo'
+  },
+  locals: { name: 'Dimitar Nikolov' }
+}
+
+const run = async (data = {}) => (
+  ah.api.specHelper.runTask('sendmail', data)
+)
+
+describe('task SendMail:run', () => {
+  beforeAll(ah.start)
+  afterAll(ah.stop)
+
+  beforeEach(async () => {
+  })
+
+  test('should send email', async () => {
+    await run(params)
+  })
+
+  test('should detect missing email option', async () => {
+    expect(await run(_.omit(params, ['mail']))).toEqual(expectedError)
+  })
+
+  test('should detect missing locals option', async () => {
+    expect(await run(_.omit(params, ['locals']))).toEqual(expectedError)
+  })
+
+  test('should detect missing template option', async () => {
+    expect(await run(_.omit(params, ['template']))).toEqual(expectedError)
+  })
+})
