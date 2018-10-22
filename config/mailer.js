eb0bc042dc9357d9b2c5d17cd8df3e93f95bed65
Dimitar Nikolov <d.nikolov@windowslive.com>
2018-10-22 22:24:02 +0300

    remove unneccesery test configuration

--- config/mailer.js
+++ config/mailer.js
@@ -23,16 +23,7 @@ exports.test = {
   mailer: (api) => {
     return {
       transport: {
-        type: 'nodemailer-mock-transport',
-        config: {
-          auth: {
-            api_key: process.env.MAILGUN_API || '97cc8efa2bed2e21bd5d306dff7ce91b-a3d67641-4c78b18a',
-            domain: process.env.MAILGUN_DOMAIN || 'sandbox1f0fe78d52b84d7984c690f5c58fa75d.mailgun.org'
-          }
-        }
-      },
-      mailOptions: {
-        from: process.env.FROM_EMAIL || 'postmaster@sandbox1f0fe78d52b84d7984c690f5c58fa75d.mailgun.org'
+        type: 'nodemailer-mock-transport'
       },
       templates: path.join(__dirname, '..', 'test/templates')
     }
