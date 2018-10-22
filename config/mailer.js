725a0710dde34acee2b411991819e0137a6fd182
Dimitar Nikolov <d.nikolov@windowslive.com>
2018-10-22 12:05:42 +0300

    moving templates folder to test as it is used only for testing. removing unused template

--- config/mailer.js
+++ config/mailer.js
@@ -15,7 +15,7 @@ exports.default = {
       mailOptions: {
         from: process.env.FROM_EMAIL || 'postmaster@sandbox1f0fe78d52b84d7984c690f5c58fa75d.mailgun.org'
       },
-      templates: path.join(__dirname, '..', 'templates')
+      templates: path.join(__dirname, '..', process.env.NODE_ENV === 'test' ? 'test/templates' : 'templates')
     }
   }
 }
