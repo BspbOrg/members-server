adce1149e4ea1bd258799786248e2745b0239949
Dimitar Nikolov <d.nikolov@windowslive.com>
2018-10-22 22:19:57 +0300

    use test configuration

--- config/mailer.js
+++ config/mailer.js
@@ -4,7 +4,7 @@ exports.default = {
   mailer: (api) => {
     return {
       transport: {
-        type: process.env.NODE_ENV === 'test' ? 'nodemailer-mock-transport' : 'nodemailer-mailgun-transport',
+        type: 'nodemailer-mailgun-transport',
         config: {
           auth: {
             api_key: process.env.MAILGUN_API || '97cc8efa2bed2e21bd5d306dff7ce91b-a3d67641-4c78b18a',
@@ -15,7 +15,26 @@ exports.default = {
       mailOptions: {
         from: process.env.FROM_EMAIL || 'postmaster@sandbox1f0fe78d52b84d7984c690f5c58fa75d.mailgun.org'
       },
-      templates: path.join(__dirname, '..', process.env.NODE_ENV === 'test' ? 'test/templates' : 'templates')
+      templates: path.join(__dirname, '..', 'templates')
+    }
+  }
+}
+exports.test = {
+  mailer: (api) => {
+    return {
+      transport: {
+        type: 'nodemailer-mock-transport',
+        config: {
+          auth: {
+            api_key: process.env.MAILGUN_API || '97cc8efa2bed2e21bd5d306dff7ce91b-a3d67641-4c78b18a',
+            domain: process.env.MAILGUN_DOMAIN || 'sandbox1f0fe78d52b84d7984c690f5c58fa75d.mailgun.org'
+          }
+        }
+      },
+      mailOptions: {
+        from: process.env.FROM_EMAIL || 'postmaster@sandbox1f0fe78d52b84d7984c690f5c58fa75d.mailgun.org'
+      },
+      templates: path.join(__dirname, '..', 'test/templates')
     }
   }
 }
