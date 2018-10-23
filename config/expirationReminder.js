edbe1dae6ec7ee28e5418bb5a60c1f98805d4229
Dimitar Nikolov <d.nikolov@windowslive.com>
2018-10-23 14:21:41 +0300

    allow env overrides for configuration

--- config/expirationReminder.js
+++ config/expirationReminder.js
@@ -2,9 +2,9 @@ exports.default = {
   expirationReminder: (api) => {
     return {
       // Frequency defines time period (in miliseconds) between two checks for expiring memberships.
-      frequency: 1000 * 60 * 60 * 24,
-      daysBeforeExpiration: 30,
-      minDaysBeforeExpiration: 5,
+      frequency: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_PERIOD || 1000 * 60 * 60 * 24,
+      daysBeforeExpiration: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_DAYS_BEFORE || 30,
+      minDaysBeforeExpiration: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_MIN_DAYS_BEFORE || 5,
       emailTemplateName: 'aboutToExpire',
       emailSubject: ''
     }
