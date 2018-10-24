56c08aff1d78b4679413d87075a2373c771f724a
Dimitar Nikolov <d.nikolov@windowslive.com>
2018-10-24 11:32:36 +0300

    support days/fraction of day in CHECK_FOR_EXPIRING_MEMBERSHIPS_PERIOD env param

--- config/expirationReminder.js
+++ config/expirationReminder.js
@@ -1,8 +1,8 @@
 exports.default = {
   expirationReminder: (api) => {
     return {
-      // Frequency defines time period (in miliseconds) between two checks for expiring memberships.
-      frequency: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_PERIOD || 1000 * 60 * 60 * 24,
+      // Frequency defines time period (in days) between two checks for expiring memberships.
+      frequency: Math.round(1000 * 60 * 60 * (24 * process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_PERIOD)) || 1000 * 60 * 60 * 24,
       daysBeforeExpiration: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_DAYS_BEFORE || 30,
       minDaysBeforeExpiration: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_MIN_DAYS_BEFORE || 5,
       emailTemplateName: 'aboutToExpire',
