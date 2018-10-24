536fd90f8767bb6f23e2284cb9226faf5bebbe75
Dimitar Nikolov <d.nikolov@windowslive.com>
2018-10-24 16:25:17 +0300

    validate expirationReminder settings before running the expirationProcessor

--- __tests__/tasks/ExpirationReminder.js
+++ __tests__/tasks/ExpirationReminder.js
@@ -14,9 +14,6 @@ describe('task expirationReminder:run', () => {
   beforeAll(ah.start)
   afterAll(ah.stop)
 
-  beforeEach(async () => {
-  })
-
   test('should start processMemberships', async () => {
     const spy = jest.spyOn(ah.api.expirationReminder, 'processMemberships')
 
@@ -32,5 +29,58 @@ describe('task expirationReminder:run', () => {
     const toDate = format(addDays(new Date(), ah.api.config.expirationReminder.daysBeforeExpiration), 'YYYY-MM-DD')
 
     expect(spy).toHaveBeenCalledWith(fromDate, toDate)
+  })
+})
+
+describe('task expirationReminder:run validations', () => {
+  const expectedError = Error('Values for minDaysBeforeExpiration and daysBeforeExpiration should be numbers greater or equal to zero')
+  afterEach(ah.stop)
+
+  test('should validate minDaysBeforeExpiration for string values', async () => {
+    await ah.start({
+      expirationReminder: (api) => {
+        return {
+          minDaysBeforeExpiration: 'some string'
+        }
+      }
+    })
+
+    await expect(ah.api.specHelper.runTask('expirationReminder')).rejects.toEqual(expectedError)
+  })
+
+  test('should validate minDaysBeforeExpiration for negative values', async () => {
+    await ah.start({
+      expirationReminder: (api) => {
+        return {
+          minDaysBeforeExpiration: -2
+        }
+      }
+    })
+
+    await expect(ah.api.specHelper.runTask('expirationReminder')).rejects.toEqual(expectedError)
+  })
+
+  test('should validate daysBeforeExpiration for string values', async () => {
+    await ah.start({
+      expirationReminder: (api) => {
+        return {
+          daysBeforeExpiration: 'some string'
+        }
+      }
+    })
+
+    await expect(ah.api.specHelper.runTask('expirationReminder')).rejects.toEqual(expectedError)
+  })
+
+  test('should validate daysBeforeExpiration for negative values', async () => {
+    await ah.start({
+      expirationReminder: (api) => {
+        return {
+          daysBeforeExpiration: -1
+        }
+      }
+    })
+
+    await expect(ah.api.specHelper.runTask('expirationReminder')).rejects.toEqual(expectedError)
   })
 })
