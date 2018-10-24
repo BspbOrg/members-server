c20d814578d3a28829d3519a29930b5fe3f6d951
Geno Roupsky <geno@roupsky.name>
2018-10-24 19:17:45 +0300

    Restyle expiration task

--- tasks/ExpirationReminder.js
+++ tasks/ExpirationReminder.js
@@ -2,33 +2,30 @@ const { api, Task } = require('actionhero')
 const addDays = require('date-fns/add_days')
 const format = require('date-fns/format')
 
+const daysValid = days => !(typeof days !== 'number' || days < 0)
+
 module.exports = class ExpirationReminder extends Task {
   constructor () {
     super()
+    this.config = api.config.expirationReminder
+
     this.name = 'expirationReminder'
     this.description = 'Sends reminder email to members for expiration'
-    this.frequency = api.config.expirationReminder.frequency
+    this.frequency = this.config.frequency
     this.queue = '*'
     this.middleware = []
 
     this.cursor = null
   }
+
   async run () {
-    if (!(this.validateConfigDays(api.config.expirationReminder.minDaysBeforeExpiration) &&
-      this.validateConfigDays(api.config.expirationReminder.daysBeforeExpiration))) {
+    if (!daysValid(this.config.minDaysBeforeExpiration) || !daysValid(this.config.daysBeforeExpiration)) {
       throw new Error('Values for minDaysBeforeExpiration and daysBeforeExpiration should be numbers greater or equal to zero')
     }
 
-    const now = format(addDays(new Date(), api.config.expirationReminder.minDaysBeforeExpiration), 'YYYY-MM-DD')
-    const expiringDate = format(addDays(new Date(), api.config.expirationReminder.daysBeforeExpiration), 'YYYY-MM-DD')
-    api.log('Check for members with expiring membership between ' + now + ' and ' + expiringDate, this.name)
-    api.expirationReminder.processMemberships(now, expiringDate)
-  }
-
-  validateConfigDays (days) {
-    if (typeof days !== 'number' || days < 0) {
-      return false
-    }
-    return true
+    const now = addDays(new Date(), this.config.minDaysBeforeExpiration)
+    const expiringDate = addDays(new Date(), this.config.daysBeforeExpiration)
+    api.log(`Check for members with expiring membership between ${format(now, 'YYYY-MM-DD')} and ${format(expiringDate, 'YYYY-MM-DD')}`, 'info')
+    await api.expirationReminder.processMemberships(now, expiringDate)
   }
 }
