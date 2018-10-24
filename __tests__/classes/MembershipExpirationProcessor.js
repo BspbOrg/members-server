3308abc56bd554306f60d2813adf4489b4c4cd94
Geno Roupsky <geno@roupsky.name>
2018-10-24 09:33:37 +0300

    Tune tests

--- __tests__/classes/MembershipExpirationProcessor.js
+++ __tests__/classes/MembershipExpirationProcessor.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-/* global jest, describe, test, expect, jest, beforeEach, afterEach */
+/* global jest, describe, test, expect, jest, beforeEach, afterEach, beforeAll, afterAll */
 
 const ah = require('../../test/ah-setup')
 const MembershipExpirationProcessor = require('../../classes/MembershipExpirationProcessor')
@@ -9,31 +9,32 @@ const addDays = require('date-fns/add_days')
 const expectedError = new Error('Provide time period!')
 const fromDate = new Date()
 
-describe('MembershipExpiration', () => {
-  const setup = () => {
-    return new MembershipExpirationProcessor({
-      api: ah.api,
-      config: {
-        daysBeforeExpiration: 30,
-        emailTemplateName: ''
-      }
-    })
-  }
-  const generateMemberAndPayment = async ({ memberOpts = { }, paymentOps = { } }) => {
-    const member = await ah.api.models.member.create(generateMember({
-      ...memberOpts
-    }))
-    await ah.api.models.payment.create(generatePayment({
-      paymentDate: new Date(),
-      paymentType: `cash`,
-      billingMemberId: member.id,
-      members: [member.id],
-      ...paymentOps
-    }))
-  }
-  beforeAll(async () => {
-    await ah.start()
+const setup = () => {
+  const processor = new MembershipExpirationProcessor({
+    api: ah.api,
+    config: {
+      daysBeforeExpiration: 30,
+      emailTemplateName: ''
+    }
   })
+  const sendMailSpy = jest.spyOn(processor, 'enqueueSendMail')
+  return { processor, sendMailSpy }
+}
+
+const generateMemberAndPayment = async ({ memberOpts = {}, paymentOps = {} }) => {
+  const member = await ah.api.models.member.create(generateMember({
+    ...memberOpts
+  }))
+  const payment = await ah.api.models.payment.create(generatePayment({
+    billingMemberId: member.id,
+    members: [member.id],
+    ...paymentOps
+  }))
+  return { member, payment }
+}
+
+describe('MembershipExpiration', () => {
+  beforeAll(ah.start)
   afterAll(ah.stop)
   beforeEach(async () => {
     await ah.api.models.payment.destroy({ where: {}, force: true })
@@ -41,114 +42,93 @@ describe('MembershipExpiration', () => {
   })
 
   describe('processing memberships for expiration', () => {
-    test('notify exipiring members with membershipEndDate in def interval', async () => {
-      const expirationProcessor = setup()
-      const sendMailSpy = jest.spyOn(expirationProcessor, 'enqueueSendMail')
-
-      await generateMemberAndPayment({ memberOpts: { membershipEndDate: addDays(fromDate, 27) } })
-      await generateMemberAndPayment({ memberOpts: { membershipEndDate: addDays(fromDate, 31) } })
-
-      await expirationProcessor.processMemberships(fromDate, addDays(fromDate, 30))
-
-      expect(sendMailSpy).toHaveBeenCalledTimes(1)
-    })
-
     test('notify expiring member with email', async () => {
-      const expirationProcessor = setup()
-      const sendMailSpy = jest.spyOn(expirationProcessor, 'enqueueSendMail')
+      const { processor, sendMailSpy } = setup()
+
+      const { member } = await generateMemberAndPayment({
+        memberOpts: { membershipEndDate: addDays(fromDate, 27) }
+      })
+
+      await processor.processMemberships(fromDate, addDays(fromDate, 30))
+
+      expect(sendMailSpy).toHaveBeenCalledWith(expect.objectContaining({ id: member.id }))
+    })
+
+    test('do not notify members outside interval', async () => {
+      const { processor, sendMailSpy } = setup()
+
+      await generateMemberAndPayment({
+        memberOpts: { membershipEndDate: addDays(fromDate, 31) }
+      })
+
+      await processor.processMemberships(fromDate, addDays(fromDate, 30))
+
+      expect(sendMailSpy).not.toHaveBeenCalled()
+    })
+
+    test('record expiration date', async () => {
+      const { processor } = setup()
+
+      const { member } = await generateMemberAndPayment({
+        memberOpts: { membershipEndDate: addDays(fromDate, 27) }
+      })
+
+      const { membershipEndDate } = member
+      expect(member.notifiedForExpiringDate).not.toEqual(membershipEndDate)
+
+      await processor.processMemberships(fromDate, addDays(fromDate, 30))
+      await member.reload()
+
+      expect(member.notifiedForExpiringDate).toEqual(membershipEndDate)
+    })
+
+    test('skip already notified', async () => {
+      const { processor, sendMailSpy } = setup()
+
+      const membershipEndDate = addDays(fromDate, 27)
 
       await generateMemberAndPayment({
         memberOpts: {
-          membershipEndDate: addDays(fromDate, 27)
+          membershipEndDate,
+          notifiedForExpiringDate: membershipEndDate
         }
       })
 
-      await expirationProcessor.processMemberships(fromDate, addDays(fromDate, 30))
+      await processor.processMemberships(fromDate, addDays(fromDate, 30))
 
-      expect(sendMailSpy).toHaveBeenCalledTimes(1)
+      expect(sendMailSpy).not.toHaveBeenCalled()
     })
 
-    test('notify expiring member with email and save date of sending mail ', async () => {
-      const expirationProcessor = setup()
+    test('skip members when payment.paymentType == "group" ', async () => {
+      const { processor, sendMailSpy } = setup()
 
       await generateMemberAndPayment({
-        memberOpts: {
-          id: 11,
-          membershipEndDate: addDays(fromDate, 27)
-        }
+        memberOpts: { membershipEndDate: addDays(fromDate, 27) },
+        paymentOps: { paymentType: 'group' }
       })
 
-      await expirationProcessor.processMemberships(fromDate, addDays(fromDate, 30))
-      const updatedMember = ah.api.models.member.findOne({ where: { id: 11 } })
+      await processor.processMemberships(fromDate, addDays(fromDate, 30))
 
-      expect(updatedMember.notifiedForExpiringDate).toEqual(updatedMember.membershipEndDate)
+      expect(sendMailSpy).not.toHaveBeenCalled()
     })
 
-    test('do not notify expiring member with email when already notified ', async () => {
-      const expirationProcessor = setup()
-      const sendMailSpy = jest.spyOn(expirationProcessor, 'enqueueSendMail')
+    test('skip members who are not billing member', async () => {
+      const { processor, sendMailSpy } = setup()
 
-      await generateMemberAndPayment({
-        memberOpts: {
-          id: 11,
-          membershipEndDate: addDays(fromDate, 27),
-          notifiedForExpiringDate: addDays(fromDate, 27)
-        }
-      })
+      const member = await ah.api.models.member.create(await generateMember({
+        membershipEndDate: addDays(fromDate, 27)
+      }))
+      const billingMember = await ah.api.models.member.create(await generateMember({
+        membershipEndDate: addDays(fromDate, 27)
+      }))
+      await ah.api.models.payment.create(generatePayment({
+        billingMemberId: billingMember.id,
+        members: [billingMember.id, member.id]
+      }))
 
-      await expirationProcessor.processMemberships(fromDate, addDays(fromDate, 30))
+      await processor.processMemberships(fromDate, addDays(fromDate, 30))
 
-      expect(sendMailSpy).toHaveBeenCalledTimes(0)
-    })
-
-    test('do not notify expiring member with payment.paymentType == "group" ', async () => {
-      const expirationProcessor = setup()
-      const sendMailSpy = jest.spyOn(expirationProcessor, 'enqueueSendMail')
-
-      await generateMemberAndPayment({
-        memberOpts: {
-          id: 11,
-          membershipEndDate: addDays(fromDate, 27)
-        },
-        paymentOps: {
-          paymentType: 'group'
-        }
-      })
-
-      await expirationProcessor.processMemberships(fromDate, addDays(fromDate, 30))
-
-      expect(sendMailSpy).toHaveBeenCalledTimes(0)
-    })
-
-    test('do not notify expiring members who are not billing member', async () => {
-      const expirationProcessor = setup()
-      const sendMailSpy = jest.spyOn(expirationProcessor, 'enqueueSendMail')
-      await ah.api.models.member.create(await generateMember({ id: 111, membershipEndDate: addDays(fromDate, 27) }))
-      await generateMemberAndPayment({
-        memberOpts: {
-          id: 11,
-          membershipEndDate: addDays(fromDate, 27)
-        },
-        paymentOps: {
-          members: [11, 111],
-          billingMemberId: 111
-        }
-      })
-
-      await expirationProcessor.processMemberships(fromDate, addDays(fromDate, 30))
-
-      expect(sendMailSpy).toHaveBeenCalledTimes(1)
-      expect(sendMailSpy).toHaveBeenCalledWith(expect.objectContaining({ id: 111 }))
-    })
-
-    test('throw error when from date is not provided ', async () => {
-      const expirationProcessor = setup()
-      expect(await expirationProcessor.processMemberships(null, new Date())).toEqual(expectedError)
-    })
-
-    test('throw error when from date is not provided ', async () => {
-      const expirationProcessor = setup()
-      expect(await expirationProcessor.processMemberships(new Date(), null)).toEqual(expectedError)
+      expect(sendMailSpy).not.toHaveBeenCalledWith(expect.objectContaining({ id: member.id }))
     })
   })
 })
