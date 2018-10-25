f22110c1f58153964e4d3f1b961b524ed52cedbb
Geno Roupsky <geno@roupsky.name>
2018-10-25 09:11:18 +0300

    Disable reminder, unless explicitly enabled through CHECK_FOR_EXPIRING_MEMBERSHIPS_PERIOD

--- config/expirationReminder.js
+++ config/expirationReminder.js
@@ -2,7 +2,7 @@ exports.default = {
   expirationReminder: (api) => {
     return {
       // Frequency defines time period (in days) between two checks for expiring memberships.
-      frequency: Math.round(1000 * 60 * 60 * (24 * process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_PERIOD)) || 1000 * 60 * 60 * 24,
+      frequency: Math.round(1000 * 60 * 60 * (24 * process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_PERIOD)) || 0,
       daysBeforeExpiration: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_DAYS_BEFORE || 30,
       minDaysBeforeExpiration: process.env.CHECK_FOR_EXPIRING_MEMBERSHIPS_MIN_DAYS_BEFORE || 5,
       emailTemplateName: 'aboutToExpire',
@@ -10,6 +10,7 @@ exports.default = {
     }
   }
 }
+
 exports.test = {
   expirationReminder: (api) => {
     return {
