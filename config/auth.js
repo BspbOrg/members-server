ad502e208d69a160bdf54ac03fe0a637f9a2b877
Daniel Petrov <dani@code6.ninja>
2018-11-06 11:43:05 +0200

    increase session ttl to 30 days

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@ exports.default = {
       bcryptComplexity: 13,
       resetTokenBytes: 64,
       resetTokenTTL: 15 * 60 * 1000,
-      excludedAttributes: [ 'password', 'passwordTime', 'resetToken', 'resetTokenTime' ],
-      sessionTtl: 60 * 60 * 24 // 1 day
+      excludedAttributes: ['password', 'passwordTime', 'resetToken', 'resetTokenTime'],
+      sessionTtl: 60 * 60 * 24 * 30 // 30 day
     }
   }
 }
