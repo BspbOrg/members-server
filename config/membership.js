22f996e0caa9a32c5bf9bbbcaa881b78e5eac50a
Geno Roupsky <geno@roupsky.name>
2018-11-07 22:32:40 +0200

    Membership configuration must be numbers

--- config/membership.js
+++ config/membership.js
@@ -2,11 +2,11 @@ exports.default = {
   membership: {
     expiringReminder: {
       // Time period (in days) between two checks for expiring memberships.
-      frequency: (process.env.EXPIRING_REMINDER_FREQUENCY || 0 /* days */) * 1000 * 60 * 60 * 24,
+      frequency: (parseFloat(process.env.EXPIRING_REMINDER_FREQUENCY) || 0 /* days */) * 1000 * 60 * 60 * 24,
       // If membership expires in less than this number of days it will consider sending reminder
-      maxDays: process.env.EXPIRING_REMINDER_MAX_DAYS || 30,
+      maxDays: parseFloat(process.env.EXPIRING_REMINDER_MAX_DAYS) || 30,
       // If membership expires in less than this number of days it will NOT send reminder
-      minDays: process.env.EXPIRING_REMINDER_MIN_DAYS || 5,
+      minDays: parseFloat(process.env.EXPIRING_REMINDER_MIN_DAYS) || 5,
       // Email template to use
       templateName: process.env.EXPIRING_REMINDER_TEMPLATE || 'membership/expiringReminder',
       // Email subject
@@ -16,11 +16,11 @@ exports.default = {
     },
     expiredNotice: {
       // Time period (in days) between two checks for expired memberships.
-      frequency: (process.env.EXPIRED_NOTICE_FREQUENCY || 0 /* days */) * 1000 * 60 * 60 * 24,
+      frequency: (parseFloat(process.env.EXPIRED_NOTICE_FREQUENCY) || 0 /* days */) * 1000 * 60 * 60 * 24,
       // If membership expired in more than this number of days it will NOT send notice
-      maxDays: process.env.EXPIRED_NOTICE_MAX_DAYS || 90,
+      maxDays: parseFloat(process.env.EXPIRED_NOTICE_MAX_DAYS) || 90,
       // If membership expired in less than this number of days it will NOT send reminder
-      minDays: process.env.EXPIRED_NOTICE_MIN_DAYS || 0,
+      minDays: parseFloat(process.env.EXPIRED_NOTICE_MIN_DAYS) || 0,
       // Email template to use
       templateName: process.env.EXPIRED_NOTICE_TEMPLATE || 'membership/expiredNotification',
       // Email subject
